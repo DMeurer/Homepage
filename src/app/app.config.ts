b27d1b16ea6ab76c3ab7803a7a046a114e8672d9
Dominik Meurer <50107525+DMeurer@users.noreply.github.com>
2025-07-19 16:51:56 +0200

    adding code block functionality

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
 import {ApplicationConfig, provideZoneChangeDetection} from "@angular/core";
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 import {provideRouter} from "@angular/router";
+import {provideHighlightOptions} from "ngx-highlightjs";
 
 import {routes} from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideAnimationsAsync()],
+	providers: [
+		provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideAnimationsAsync(),
+		provideHighlightOptions({
+			lineNumbersLoader: () => import("ngx-highlightjs/line-numbers"),
+			coreLibraryLoader: () => import("highlight.js/lib/core"),
+			/* This would only load the languages you need, reducing bundle size.
+			languages: {
+				typescript: () => import("highlight.js/lib/languages/typescript"),
+				css: () => import("highlight.js/lib/languages/css"),
+				xml: () => import("highlight.js/lib/languages/xml"),
+			},
+			 */
+			themePath: "assets/styles/androidstudio.css",
+			// themePath: "assets/styles/github-dark.css",
+		}),
+	],
 };
