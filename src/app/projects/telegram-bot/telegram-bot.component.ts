709a9d0d7f44403d087f62e2b2b3718682e6a71d
Dominik Meurer <dom.meurer@gmx.de>
2025-07-19 16:51:56 +0200

    adding code block functionality

--- src/app/projects/telegram-bot/telegram-bot.component.ts
+++ src/app/projects/telegram-bot/telegram-bot.component.ts
@@ -1,11 +1,16 @@
 import {Component} from "@angular/core";
+import {Highlight} from "ngx-highlightjs";
+import {HighlightLineNumbers} from "ngx-highlightjs/line-numbers";
 
 @Component({
 	selector: "app-telegram-bot",
-	imports: [],
+	imports: [
+		Highlight,
+		HighlightLineNumbers,
+	],
 	templateUrl: "./telegram-bot.component.html",
 	styleUrl: "./telegram-bot.component.scss",
 })
 export class TelegramBotComponent {
-	
+	protected code = "import { Injectable } from '@angular/core';"
 }
