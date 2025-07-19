9c1257659a62cae25c18db02b809cbc1e21dd236
Dominik Meurer <dom.meurer@gmx.de>
2025-07-19 14:54:12 +0200

    adding a cover to the blog post

--- src/common/data/blogPosts/BLOG_POSTS.ts
+++ src/common/data/blogPosts/BLOG_POSTS.ts
@@ -7,7 +7,7 @@ export const BLOG_POSTS: Project[] = [
 		tag: "r2d2/two-wheeled-movement",
 		name: "Simulating two wheeled movement",
 		description: "Simulating the movement of a R2D2-like robot",
-		image: "",
+		image: "/blog/r2d2/two-wheel-movement/cover.png",
 		date: new Date("2023-10-01"),
 		chips: [
 			{
