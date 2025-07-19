31aa91ac35b051517bf73be9d45c6ea9e28cbc85
Dominik Meurer <dom.meurer@gmx.de>
2025-07-19 18:25:27 +0200

    fixing code blocks and finalize the telegram bot page

--- src/app/projects/telegram-bot/telegram-bot.component.ts
+++ src/app/projects/telegram-bot/telegram-bot.component.ts
@@ -1,16 +1,22 @@
+import {AsyncPipe} from "@angular/common";
 import {Component} from "@angular/core";
 import {Highlight} from "ngx-highlightjs";
 import {HighlightLineNumbers} from "ngx-highlightjs/line-numbers";
+import {HighlightPlusModule} from "ngx-highlightjs/plus";
 
 @Component({
 	selector: "app-telegram-bot",
 	imports: [
-		Highlight,
 		HighlightLineNumbers,
+		Highlight,
+		HighlightPlusModule,
+		AsyncPipe,
 	],
 	templateUrl: "./telegram-bot.component.html",
 	styleUrl: "./telegram-bot.component.scss",
 })
 export class TelegramBotComponent {
-	protected code = "import { Injectable } from '@angular/core';"
+	protected codeUrlCronjob = "https://raw.githubusercontent.com/DMeurer/go-telegram-bot/refs/heads/main/deployment/cronjob.sh";
+	protected codeUrlCheckUpdates = "https://raw.githubusercontent.com/DMeurer/go-telegram-bot/refs/heads/main/deployment/check-updates.sh";
+	protected codeUrlDeploy = "https://raw.githubusercontent.com/DMeurer/go-telegram-bot/refs/heads/main/deployment/deploy.sh";
 }
